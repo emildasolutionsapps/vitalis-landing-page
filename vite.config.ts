3d521f7eed18264e8c87f122f0c048ccbb0d76b1
emildasolutionsapps <Emildasolutionsapps@gmail.com>
2025-07-31 10:09:19 +0530

    groot Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/landingpage/',            // 👈  NEW
   plugins: [react()],
-  optimizeDeps: { exclude: ['lucide-react'] },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
